0ae786f7f0a11aed27b278cfa354d4aebb92e72d
jonazeiselt <jonaseiselt@hotmail.com>
2019-01-29 14:09:46 +0100

    .gitignore should now work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,31 @@
-﻿module.exports = function (grunt) {
+﻿// Wrapper function containing Grunt code
+module.exports = function (grunt) {
+    // Define configuration data
     grunt.initConfig({
+        // Import JSON metadata into grunt config
         pkg: grunt.file.readJSON("package.json"),
         protractor: {
             options: {
-                configFile: "node_modules/protractor/example/conf.js",
-                keepAlive: true,
-                noColor: false
+                configFile: "node_modules/protractor/example/conf.js", // Default config file
+                keepAlive: true, // Don't stop grunt process when a test fails
+                noColor: false, // Make protractor use colors in its output 
+                args: {
+                    // Pass arguments to the command
+                }
             },
             e2e: {
                 options: {
-                    configFile: "tests/protractor.config.js"
+                    configFile: "tests/protractor.config.js",
+                    args: {} // Target-specific arguments
                 }
+          
             }
         }
     });
 
+    // Enable plugin
     grunt.loadNpmTasks('grunt-protractor-runner');
-    grunt.registerTask('default', ['protractor:e2e']);
+
+    // npm run test (test comes from package.json's "test" : "grunt test")
+    grunt.registerTask('test', ['protractor:e2e']);
 };
--- todo-jenkins.bat
+++
@@ -1,6 +0,0 @@
-﻿
-cd C:\Users\jeisel\Desktop\Testautomatisering\TodoNg\TodoNg
-call npm install -g grunt-cli
-call npm install grunt
-call npm install grunt-protractor-runner
-grunt
